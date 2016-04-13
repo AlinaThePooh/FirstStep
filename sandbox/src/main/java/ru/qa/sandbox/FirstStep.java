7211ac0c55790a5d0d6f887dc934e7d822af2d7f
AlinaThePooh <gefter.alina@gmail.com>
2016-04-13 23:28:58 +0300

    Several functions are added

--- sandbox/src/main/java/ru/qa/sandbox/FirstStep.java
+++ sandbox/src/main/java/ru/qa/sandbox/FirstStep.java
@@ -3,11 +3,26 @@ package ru.qa.sandbox;
 public class FirstStep {
 
 	public static void main(String[] args) {
-	  System.out.println("Hello, world");
+	  hello("user");
 
-        int l = 6;
-        int s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double s) {
+        return s*s;
+    }
+
+    public static double area(double a, double b) {
+        return a*b;
     }
 
 }
