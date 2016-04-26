60f764bb7537d5ec3c6d0c88c2adddaf0c49ba0c
AlinaThePooh <gefter.alina@gmail.com>
2016-04-27 00:23:42 +0300

    Homework - TestNG and first tests

--- sandbox/src/test/java/ru/qa/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/qa/sandbox/PointTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 /**
  * Created by user on 19.04.2016.
  */
-public class PointTestSuccess {
+public class PointTest {
 
     @Test
     public void testDistance_zero() {
@@ -14,6 +14,13 @@ public class PointTestSuccess {
         Point point_2 = new Point(1, 3);
         Assert.assertEquals(point_1.distance(point_2), 0.0);
 
+    }
+    @Test
+    public void testDistance() {
+
+        Point point_3 = new Point (0,5);
+        Point point_4 = new Point (1,3);
+        Assert.assertEquals(point_3.distance(point_4), 160.0);
     }
 
 }
