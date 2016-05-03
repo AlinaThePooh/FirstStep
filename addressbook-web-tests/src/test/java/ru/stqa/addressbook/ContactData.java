a81b3a3f09baeee0b460998b96ada3b33db6e104
AlinaThePooh <gefter.alina@gmail.com>
2016-05-04 01:00:10 +0300

    Test for Contact Creation is added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package ru.stqa.addressbook;
+
+public class ContactData {
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final String phone;
+    private final String mail;
+
+    public ContactData(String name, String surname, String address, String phone, String mail) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phone = phone;
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+}
