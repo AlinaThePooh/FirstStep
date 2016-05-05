2646ca679ed9ef9367f686b05c3e869e41b5cd3f
AlinaThePooh <gefter.alina@gmail.com>
2016-05-05 23:56:09 +0300

    Homework 5

--- addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbook/appmanager/ContactHelper.java
@@ -7,36 +7,26 @@ import ru.stqa.addressbook.model.ContactData;
 /**
  * Created by user on 05.05.2016.
  */
-public class ContactHelper {
-    private FirefoxDriver wd;
+public class ContactHelper extends HelperBase{
 
     public ContactHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super (wd);
     }
 
     public void submitContact() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+        click(By.xpath("//div[@id='content']/form/input[21]"));
     }
 
     public void fillContactForm(ContactData contactData) {
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(contactData.getMail());
+        type(By.name("firstname"),contactData.getName());
+        type(By.name("lastname"),contactData.getSurname());
+        type(By.name("address"),contactData.getAddress());
+        type(By.name("mobile"),contactData.getPhone());
+        type(By.name("email"),contactData.getMail());
+
     }
 
     public void goToAddContactPage() {
-        wd.findElement(By.linkText("add new")).click();
+        click(By.linkText("add new"));
     }
 }
